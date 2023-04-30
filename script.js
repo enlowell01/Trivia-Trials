7faf6b4809a7fd9ecd0d0860fe19f439903c222f
Evan <evanlowell01@gmail.com>
2023-04-30 19:36:24 -0400

    Updates functions and adds new citation

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let prompts = document.getElementsByClassName("answer-label")
-let answers = document.getElementsByClassName("answer-choice")
-
-
 class Question {
     constructor(prompt, answer, option0, option1, option2, option3) {
         this.prompt = prompt;
@@ -14,48 +10,103 @@ class Question {
 }
 
 let questionsList = [];
+let score = 0;
 
-let q1 = new Question("What is 1 + 2?", 3, 5, 4, 3, 8)
+let q1 = new Question("What is 1 + 2?", "3", "5", "4", "3", "8")
 let q2 = new Question("What does a cow drink?", "Milk", "Water", "Milk", "Orange Juice", "Soda")
+let q3 = new Question("What continent is also an island?", "Australia", "North America", "South America", "Africa", "Australia")
 
-questionsList.push[q1, q2];
-
+questionsList.push(q1, q2, q3);
+console.log(questionsList);
 
 function renderOptions (questionVariable) {
+    let prompts = document.getElementsByClassName('answer-label')
+    let answers = document.getElementsByClassName('answer-choice')
+
     document.getElementById("question-text").innerHTML = questionVariable.prompt;
+
+    for (let i = 0; i < prompts.length; i++) {
+        prompts[i].style.backgroundColor = '#00beff';
+    }
+
+    /* JavaScript insertAfter. javascripttutorial.net. https://www.javascripttutorial.net/javascript-dom/javascript-insertafter/ */ 
+    function insertAfter(newNode, existingNode) {
+        existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+    }
+
     for (let i = 0; i < prompts.length; i++) {
         Object.entries(questionVariable).forEach(([propName, propValue]) => {
             if (propName.includes(i)) {
-                prompts[i].appendChild(document.createTextNode(propValue))
+                if (prompts[i].childNodes.length > 1) {
+                    prompts[i].lastChild.remove();
+                    insertAfter(document.createTextNode(propValue), prompts[i].lastChild)
+                } else {
+                    insertAfter(document.createTextNode(propValue), prompts[i].lastChild)
+                }
+                answers[i].setAttribute("value", "")
+                console.log(answers[i].value)
                 answers[i].setAttribute("value", propValue)
+                console.log(answers[i].value)
+                if (answers[i].value == questionVariable.answer) {
+                    console.log('yes')
+                }
             }
         })
     }
-    function answerSelection () {
+
+    let completionStatus = false;
+    let currentIndex = questionsList.indexOf(questionVariable);
+    
+    function continueQuiz (ready) {
+        if (completionStatus && ready) {
+            currentIndex++;
+            console.log(currentIndex);
+            renderOptions(questionsList[currentIndex])
+        }
+    }
+
+    function answerSelection() {
         for(let i = 0; i < answers.length; i++) {
             answers[i].addEventListener('change', function() {
-                if (answers[i].checked == true) {
-                    const confirmation = confirm("Are you sure?")
-                    if (confirmation) {
-                        answers[i].parentNode.style.backgroundColor = '#24146d';
-                        if (answers[i].value == questionVariable.answer) {
-                            alert("Good job!")
-                            return true;
-                        } else {
-                            alert("Sorry, no cigar!")
-                        }
-                    } 
-                }
+                const confirmation = confirm("Are you sure?")
+                if (confirmation) {
+                    if (answers[i].checked == true && answers[i].value == questionVariable.answer) {
+                        console.log('good');        
+                        alert("Good job!")
+                        score++;
+                        completionStatus = true;
+                        console.log(completionStatus);
+                        console.log(answers[i].value)
+                        let readyStatus = false;
+                        setTimeout(() => {
+                            readyStatus = confirm("Are you ready for the next question?")
+                            if (readyStatus) {
+                                continueQuiz(readyStatus)
+                            }
+                        }, 1000)
+                        answers[i].parentNode.style.backgroundColor = '#00ff00';
+                    } else if (answers[i].checked == true && answers[i].value !== questionVariable.answer){
+                        alert("Sorry, no cigar!")
+                        completionStatus = true;
+                        console.log(completionStatus);
+                        console.log(answers[i].value)
+                        let readyStatus = false;
+                        setTimeout(() => {
+                            readyStatus = confirm("Are you ready for the next question?")
+                            if (readyStatus) {
+                                continueQuiz(readyStatus)
+                            }
+                        }, 1000)
+                        answers[i].parentNode.style.backgroundColor = '#cc0000';
+                    }
+                } 
             })
         }
     }
     answerSelection();
-    /*if (answerSelection == true) {
-        renderOptions(questionsList.next)
-    }*/
 }
 
-renderOptions(questionsList[0])
+renderOptions(questionsList[0]);
 
 /*for (let i = 0; i < answers.length; i++) {
     answers[i].addEventListener('change', function () {
@@ -87,9 +138,114 @@ renderOptions(questionsList[0])
     })
 })*/
 
+/*function answerSelection() {
+    for(let i = 0; i < answers.length; i++) {
+        answers[i].addEventListener('change', function() {
+            const confirmation = confirm("Are you sure?")
+            if (confirmation) {
+                if (answers[i].checked == true) {
+                    const confirmation = confirm("Are you sure?")
+                    if (confirmation) {
+                        answers[i].parentNode.style.backgroundColor = '#d30fa2';
+                        if (answers[i].value == questionVariable.answer) {
+                            alert("Good job!")
+                            score++;
+                            return true;
+                        } else {
+                            alert("Sorry, no cigar!")
+                            return true;
+                        }
+                    } 
+                }
+        })
+    }
+}*/
 
 
+/*function renderOptions (questionVariable) {
 
+    document.getElementById("question-text").innerHTML = questionVariable.prompt;
+
+    for (let i = 0; i < prompts.length; i++) {
+        prompts[i].style.backgroundColor = '#00beff';
+    }
+
+    JavaScript insertAfter. javascripttutorial.net. https://www.javascripttutorial.net/javascript-dom/javascript-insertafter/ 
+    function insertAfter(newNode, existingNode) {
+        existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+    }
+
+    for (let i = 0; i < prompts.length; i++) {
+        Object.entries(questionVariable).forEach(([propName, propValue]) => {
+            if (propName.includes(i)) {
+                if (prompts[i].childNodes.length > 1) {
+                    prompts[i].lastChild.remove();
+                    insertAfter(document.createTextNode(propValue), prompts[i].lastChild)
+                } else {
+                    insertAfter(document.createTextNode(propValue), prompts[i].lastChild)
+                }
+                answers[i].setAttribute("value", "")
+                console.log(answers[i].value)
+                answers[i].setAttribute("value", propValue)
+                console.log(answers[i].value)
+                if (answers[i].value == questionVariable.answer) {
+                    console.log('yes')
+                }
+            }
+        })
+    }
+
+    let completionStatus = false;
+    let currentIndex = questionsList.indexOf(questionVariable);
+    
+    function continueQuiz (ready) {
+        if (completionStatus && ready) {
+            currentIndex++;
+            console.log(currentIndex);
+            renderOptions(questionsList[currentIndex])
+        }
+    }
+
+    function answerSelection() {
+        for(let i = 0; i < answers.length; i++) {
+            answers[i].addEventListener('change', function() {
+                const confirmation = confirm("Are you sure?")
+                if (confirmation) {
+                    if (answers[i].checked == true && answers[i].value == questionVariable.answer) {
+                        console.log('good');        
+                        alert("Good job!")
+                        score++;
+                        completionStatus = true;
+                        console.log(completionStatus);
+                        console.log(answers[i].value)
+                        let readyStatus = false;
+                        setTimeout(() => {
+                            readyStatus = confirm("Are you ready for the next question?")
+                            if (readyStatus) {
+                                continueQuiz(readyStatus)
+                            }
+                        }, 1000)
+                        answers[i].parentNode.style.backgroundColor = '#00ff00';
+                    } else if (answers[i].checked == true && answers[i].value !== questionVariable.answer){
+                        alert("Sorry, no cigar!")
+                        completionStatus = true;
+                        console.log(completionStatus);
+                        console.log(answers[i].value)
+                        let readyStatus = false;
+                        setTimeout(() => {
+                            readyStatus = confirm("Are you ready for the next question?")
+                            if (readyStatus) {
+                                continueQuiz(readyStatus)
+                            }
+                        }, 1000)
+                        answers[i].parentNode.style.backgroundColor = '#cc0000';
+                    }
+                } 
+            })
+        }
+    }
+    answerSelection();
+}*/
 
 
 
